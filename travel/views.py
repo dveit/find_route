a91914b1b2c39997aae81d43287dc7811812433b
Dmitry Veit <veit.dmitry@protonmail.com>
2021-10-19 21:43:28 +0400

    added template inheritance

--- travel/views.py
+++ travel/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     name = 'Dmitry'
     return render(request, 'home.html', {'name': name})
+
+def about(request):
+    text = 'About us'
+    return render(request, 'about.html', {'text': text})

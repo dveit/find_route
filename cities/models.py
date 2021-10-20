edaaf1ae2524a78479f2d0323f43394e7c34ca6e
Dmitry Veit <veit.dmitry@protonmail.com>
2021-10-20 22:10:29 +0400

    added cities

--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Город')
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = 'город'
+        verbose_name_plural = 'города'
+        ordering = ['name']
--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from cities.models import City
 
 # Create your views here.
+
+__all__ = (
+    'home',
+)
+
+def home(request):
+    cities = City.objects.all()
+    context = {'objects_list': cities}
+    return render(request, 'cities/home.html', context)

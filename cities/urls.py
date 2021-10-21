c74fb8df818d82613f0add1c00e3c98c45312006
Dmitry Veit <veit.dmitry@protonmail.com>
2021-10-21 23:31:04 +0400

    minor changes

--- cities/urls.py
+++ cities/urls.py
@@ -3,4 +3,5 @@ from cities.views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('<int:pk>/',home, name='home'),
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from cities.models import City
 
 # Create your views here.
@@ -7,7 +7,11 @@ __all__ = (
     'home',
 )
 
-def home(request):
+def home(request, pk=None):
+    if pk:
+        city = get_object_or_404(City, id=pk)
+        context = {'object': city}
+        return render(request, 'cities/detail.html', context)
     cities = City.objects.all()
     context = {'objects_list': cities}
     return render(request, 'cities/home.html', context)

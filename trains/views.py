58edd541b0979c4738ed83878cea51bbf513528e
Dmitry Veit <veit.dmitry@protonmail.com>
2021-10-23 21:42:13 +0400

    added trains editing and dropdown menu

---
+++ trains/views.py
@@ -0,0 +1,57 @@
+from django.db.models import query
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse_lazy
+from django.core.paginator import Paginator
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView, ListView
+
+from trains.models import Train
+from trains.forms import TrainForm
+
+# Create your views here.
+
+__all__ = (
+    'home', 'TrainListView', 'TrainDetailView', 
+    'TrainCreateView','TrainUpdateView', 'TrainDeleteView', 
+)
+
+def home(request, pk=None):
+    trains = Train.objects.all()
+    content_list = Paginator(trains, 2)
+    page_number = request.GET.get('page')
+    page_obj = content_list.get_page(page_number)
+    context = {'page_obj': page_obj,}
+    return render(request, 'trains/home.html', context)
+
+class TrainListView(ListView):
+    paginate_by = 5
+    model = Train
+    template_name = 'trains/home.html'
+
+class TrainDetailView(DetailView):
+    queryset = Train.objects.all()
+    template_name = 'trains/detail.html'
+
+class TrainDeleteView(DeleteView):
+    model = Train
+    # template_name = 'trains/delete.html'
+    success_url = reverse_lazy('trains:home')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(request, "Поезд успешно удален")
+        return self.post(request, *args, **kwargs)
+
+class TrainCreateView(SuccessMessageMixin, CreateView):
+    model = Train
+    form_class = TrainForm
+    template_name = 'trains/create.html'
+    success_url = reverse_lazy('trains:home')
+    success_message = "Поезд успешно создан"
+
+class TrainUpdateView(SuccessMessageMixin, UpdateView):
+    model = Train
+    form_class = TrainForm
+    template_name = 'trains/update.html'
+    success_url = reverse_lazy('trains:home')
+    success_message = "Поезд успешно отредактирован"
